b785a9ea92ea11398f3960feaee45ada1e978d0f
An Yang <anyang0314@yahoo.com>
2015-06-05 20:09:08 +0800

    Create a bat for using MarkdownPad to open md file
    This is the begining

---
+++ openmd.bat
@@ -0,0 +1,5 @@
+@start /b D:\program\markdownpad2-portable\MarkdownPad2.exe %1
+echo opening %1...
+@Rem This is a short command for MarkDown
+@Rem "start /b" make the program run in background
+@Rem "%1"  represent a parameter
